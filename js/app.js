0faa30e32af809a6cdcc3142291ccb54227c76d2
Andres R. Bucheli <radio_seattle@hotmail.com>
2022-11-18 18:33:16 +0300

    editing the h3s

--- js/app.js
+++ js/app.js
@@ -8,6 +8,6 @@ const app = new Vue({
         'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.', 
         'Today I start learning Vue. I got this.'
       ],
-      bio: "Hi, I’m Tomchik! I'm a web developer with in-depth experience in UI/UX design. In a nutshell, I create websites that help organizations address business challenges and meet their needs."
+      bio: "Hi, I'm a web developer with in-depth experience in UI/UX design. In a nutshell, I create websites that help organizations address business challenges and meet their needs."
     }
   });
