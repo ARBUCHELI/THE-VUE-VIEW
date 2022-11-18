7a26a4beca50e394aa9e7fd0c7f91d6554ebbc0c
Andres R. Bucheli <radio_seattle@hotmail.com>
2022-11-18 17:50:02 +0300

    changing profile picture

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 const app = new Vue({
     el: '#app',
     data: {
-      username: 'JSNINJA',
+      username: 'JSDEV',
       newTweet: '',
       tweets: [
         'Started learning to code today. Wish me luck!', 
