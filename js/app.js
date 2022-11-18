87eef2d07a3c50309df34b16ecbd5051d05a25c7
Andres R. Bucheli <radio_seattle@hotmail.com>
2022-11-18 17:57:04 +0300

    editing bio

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const app = new Vue({
     el: '#app',
     data: {
-      username: 'JSDEV',
+      username: 'DEVNINJA',
       newTweet: '',
       tweets: [
         'Started learning to code today. Wish me luck!', 
         'Okay, I learned HTML, CSS, and JavaScript. But, how do I combine them together?? Send help.', 
         'Today I start learning Vue. I got this.'
       ],
-      bio: 'Excited future front-end  engineer.'
+      bio: "Hi, I’m Tomchik! I'm a web developer with in-depth experience in UI/UX design. In a nutshell, I create websites that help organizations address business challenges and meet their needs. I manage everything from website navigation and layout to a company's web hosting and security architecture. My expertise lies within front-end web apps, and the main languages in my tech stack are JavaScript, React, and of course HTML/CSS. I’m a lifelong learner (currently taking a course on building AI chatbots with Python!) and love to read, run, and find new bubble tea shops all over the world."
     }
   });
